7f4c2033af6f2ac3d263d4314efa1f0c1504c465
Vishal Kartik <88201850+karvis9@users.noreply.github.com>
2022-09-17 21:04:41 -0700

    Adding Letters prefab and floating them
    Floating the letters
    Adding initial 4 alphabets and top, left and right walls

---
+++ Assets/scripts/Bubble.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Vector3 force;
+    [SerializeField] Sprite[] bubbleSprites;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = bubbleSprites[Random.Range(0, 4)];
+        force = new Vector3(Random.Range(-50, 50), Random.Range(50, 100) ,0);
+        rb.AddForce(force);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "TopWall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
