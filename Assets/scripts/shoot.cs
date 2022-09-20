90d05fc2676b24923330ad8e3293d84e395542a4
prashantplusplus <guptapra@usc.edu>
2022-09-19 19:23:45 -0700

    PowerBar
    Added PowerBar

--- Assets/scripts/shoot.cs
+++ Assets/scripts/shoot.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class shoot : MonoBehaviour
 {
     public float LaunchForce;
+    private float MaxLaunchForce = 700;
     public GameObject Arrow;
     public bool spaceDown = true;
+    public Image powerBar;
+    private IEnumerator UpdatePowerBarCoRoutine;
+    
     // Start is called before the first frame update
     void Start()
     {
         LaunchForce = 200;
+        UpdatePowerBarCoRoutine = UpdatePowerBar();
     }
 
     // Update is called once per frame
@@ -19,20 +25,41 @@ public class shoot : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             LaunchForce += 400 * Time.deltaTime;
+            StartCoroutine(UpdatePowerBarCoRoutine);
         }
+        if(Input.GetKey("mouse 0"))
+        {
+            LaunchForce += 400 * Time.deltaTime;
+            StartCoroutine(UpdatePowerBarCoRoutine);
+        }
+
         if(Input.GetKeyUp(KeyCode.Space))
         {
-            LaunchForce = Mathf.Min(700,LaunchForce);
+            LaunchForce = Mathf.Min(MaxLaunchForce,LaunchForce);
             Shoot();
+            powerBar.fillAmount = 0;
+            LaunchForce = 200;
+            StopCoroutine(UpdatePowerBarCoRoutine);
         }
 
         if(Input.GetKeyUp("mouse 0"))
         {
-            LaunchForce = 600;
+            LaunchForce = Mathf.Min(MaxLaunchForce,LaunchForce);
             Shoot();
+            powerBar.fillAmount = 0;
+            LaunchForce = 200;
+            StopCoroutine(UpdatePowerBarCoRoutine);
         }
     }
 
+    IEnumerator UpdatePowerBar(){
+        while(true){
+            powerBar.fillAmount = (LaunchForce-200)/(MaxLaunchForce-200);
+            Debug.Log(LaunchForce/MaxLaunchForce);
+            yield return new WaitForSeconds(0.06f);
+        }
+        yield return null;
+    }
     void Shoot()
     {
         GameObject ArrowIns = Instantiate(Arrow, transform.position, transform.rotation);
