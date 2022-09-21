83e10dce9de370baf20411a701a4612ea71882cd
kommakarthikreddy <komma@usc.edu>
2022-09-21 16:54:23 -0700

    Update spwaning logic

--- Assets/scripts/LetterSpawner.cs
+++ Assets/scripts/LetterSpawner.cs
@@ -6,26 +6,25 @@ public class LetterSpawner : MonoBehaviour
 {
     //[SerializeField] GameObject[] letterPrefab;
     public List<GameObject> letterPrefab; /*** Extend this list to add letters ***/
-    public Queue<char> spawnList;
+    public List<char> spawnList;
+    public List<char> duplicateCharList;
 
+    public HashSet<char> actualSet;
+    public HashSet<char> duplicateSet;
+    public int count;
+    public static int totalCharsEverySec = 2; 
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnList = new Queue<char>();
-        // spawnList.Enqueue('A');
-        // spawnList.Enqueue('A');
-        // spawnList.Enqueue('A');
-        // spawnList.Enqueue('A');
-        // spawnList.Enqueue('A');
-        // spawnList.Enqueue('A');
-        // spawnList.Enqueue('A');
-        // spawnList.Enqueue('B');
-        // spawnList.Enqueue('B');
-        // spawnList.Enqueue('B');
-        // spawnList.Enqueue('B');
-        InvokeRepeating("PopSpawn", 0.0f, 1f);
+        spawnList = new List<char>();
+        duplicateCharList = new List<char> ();
 
+        actualSet = new HashSet<char> ();
+        duplicateSet = new HashSet<char> ();
+
+        count = 0;
+        InvokeRepeating("PopSpawn", 0.0f, 2f);
     }
 
     // Update is called once per frame
@@ -33,16 +32,58 @@ public class LetterSpawner : MonoBehaviour
     {
         
     }
+
+    void Initialize ()
+    {
+        int word_len = WordBlanks.wb.word.Length;
+
+        for (int i = 0; i < word_len; i++) {
+            spawnList.Add (WordBlanks.wb.word[i]);
+            actualSet.Add (WordBlanks.wb.word[i]);
+            Debug.Log ("letter " + WordBlanks.wb.word[i]);
+        }
+
+        while (duplicateSet.Count < word_len) {
+            int random_num = UnityEngine.Random.Range(0, 26);
+            char alphabet = (char)((int) 'a' + random_num);
+
+            if (!actualSet.Contains (alphabet)) {
+                duplicateSet.Add (alphabet);
+                duplicateCharList.Add (alphabet);
+            }
+        }
+    }
+
     //pops an character from list and spawns it
     void PopSpawn() 
     {
-        if(spawnList.Count == 0) {
-            // Randomly spawns from List
-            GameObject go = Instantiate(letterPrefab[Random.Range(0, letterPrefab.Count)], transform.position, transform.rotation);
+
+        if (count == 0) {
+            Initialize ();
+        }
+
+        // randomly decide no of actual chars to spwan
+        int no_actual = UnityEngine.Random.Range(1, totalCharsEverySec);
+        int word_len = WordBlanks.wb.word.Length;
+
+        // spawn the actual chars based on above random number
+        for (int i = 0; i < no_actual; i++) {
+
+            int idx = UnityEngine.Random.Range(0, word_len);
+            char alphabet = spawnList[idx];
+            int prefab_index = (int) alphabet - (int) 'a';
+            
+            GameObject go = Instantiate(letterPrefab[prefab_index], transform.position, transform.rotation);
             go.SetActive(true);
         }
-        else {
-            GameObject go = Instantiate(letterPrefab[spawnList.Dequeue() - 'A'], transform.position, transform.rotation);
+
+        // spawn non-actual chars based on totalCharsEverySec
+        for (int i = no_actual; i < totalCharsEverySec; i++) {
+            int idx = UnityEngine.Random.Range(0, word_len);
+            char alphabet = duplicateCharList[idx];
+            int prefab_index = (int) alphabet - (int) 'a';
+
+            GameObject go = Instantiate(letterPrefab[prefab_index], transform.position, transform.rotation);
             go.SetActive(true);
         }
     }
