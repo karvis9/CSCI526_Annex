a95d53b45b33a377033851111a62551b49a25b88
prashantplusplus <81324922+prashantplusplus@users.noreply.github.com>
2022-10-09 14:48:20 -0700

    Change Scene

--- Assets/scripts/Level_Zero.cs
+++ Assets/scripts/Level_Zero.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level_Zero : MonoBehaviour
 {
+    public int CharsLength;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //instantiate player
+        //get count of bubbles to hit
+        //CharsLength = GameObject.FindGameObjectsWithTag("Letter").Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameObject.FindGameObjectsWithTag("Letter").Length == 0)
+            SceneManager.LoadScene("Level_2");
     }
+
+
 }
