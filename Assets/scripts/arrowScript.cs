2053e7301e2115c84fd62e42cb297fd49cbdc7fd
rchanumo <scrs.ravi14@gmail.com>
2022-10-10 03:48:03 -0400

    add bomb logic

--- Assets/scripts/arrowScript.cs
+++ Assets/scripts/arrowScript.cs
@@ -62,6 +62,10 @@ public class arrowScript : MonoBehaviour
         if(collision.gameObject.name == "RightWall"){
             Destroy(this.gameObject);
         }
+        if (collision.gameObject.tag == "bomb")
+        {
+            Destroy(this.gameObject);
+        }
         //Debug.Log(ScoreManager.sm.getCurrentPoint());
         //Destroy(this.gameObject);
     }
