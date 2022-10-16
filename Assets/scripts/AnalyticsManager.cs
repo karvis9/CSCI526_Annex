ca1e45fa179855a07aedc1cc450b2de43c7520a2
mzhmzh <51972727+mzhmzh@users.noreply.github.com>
2022-10-15 20:15:17 -0700

    level added for analytics

--- Assets/scripts/AnalyticsManager.cs
+++ Assets/scripts/AnalyticsManager.cs
@@ -37,30 +37,34 @@ public class AnalyticsManager : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
-        if (sceneName.Equals("Level_0"))
-        {
-            return;
-        }
 
         WWWForm form = new WWWForm();
         form.AddField("entry.880690018", _sessionID);
         form.AddField("entry.1107566471", WordBlanks.category);
+        form.AddField("entry.1970322068", sceneName);
 
         if (eventType.Equals("Char Revealed"))
         {
+            if (!sceneName.Equals("Level_0"))
+            {
+                form.AddField("entry.19206144", ScoreManager.sm.getFinalScore());
+                form.AddField("entry.1514343154", CountDownTimer.countDownTimerObj.getTimeLeft());
+                form.AddField("entry.1890577055", shoot.shootController.getArrowsCount() - previous_charArrowCount);
+            }
             eventType = eventType + " " + (WordBlanks.wb.word.Length - WordBlanks.wb.maskedCnt);
             form.AddField("entry.890767811", WordBlanks.wb.getWord().Length);
-            form.AddField("entry.19206144", ScoreManager.sm.getFinalScore());
-            form.AddField("entry.1514343154", CountDownTimer.countDownTimerObj.getTimeLeft());
-            form.AddField("entry.1890577055", shoot.shootController.getArrowsCount() - previous_charArrowCount);
             previous_charArrowCount = shoot.shootController.getArrowsCount();
         }
         else if (!eventType.Equals("Game Start"))
         {
             form.AddField("entry.890767811", WordBlanks.wb.getWord().Length);
-            form.AddField("entry.19206144", ScoreManager.sm.getFinalScore());
-            form.AddField("entry.1514343154", CountDownTimer.countDownTimerObj.getTimeLeft());
-            form.AddField("entry.1890577055", shoot.shootController.getArrowsCount());
+            if (!sceneName.Equals("Level_0"))
+            {
+                form.AddField("entry.19206144", ScoreManager.sm.getFinalScore());
+                form.AddField("entry.1514343154", CountDownTimer.countDownTimerObj.getTimeLeft());
+                form.AddField("entry.1890577055", shoot.shootController.getArrowsCount());
+                return;
+            }
         }
 
         form.AddField("entry.1308275481", eventType);
