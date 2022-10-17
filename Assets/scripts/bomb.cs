490f75599c036f8db6904dc6d728bb22dee9cf1d
rchanumo <scrs.ravi14@gmail.com>
2022-10-16 23:40:11 -0400

    fixed bomb spawning

--- Assets/scripts/bomb.cs
+++ Assets/scripts/bomb.cs
@@ -13,7 +13,7 @@ public class bomb : MonoBehaviour
     void Start()
     {
         ps = Animator.gameObject.GetComponent<ParticleSystem>();
-        transform.position = new Vector3(Random.Range(8f, 9f), Random.Range(-2.0f, 3.5f), transform.position.z);
+        //transform.position = new Vector3(Random.Range(8f, 9f), Random.Range(-2.0f, 3.5f), transform.position.z);
     }
 
     // Update is called once per frame
@@ -61,10 +61,10 @@ public class bomb : MonoBehaviour
                 }
             }
             ps.Play();
+            Destroy(this.gameObject, 0.2f);
         }
         //ParticleSystem ps = Animator.gameObject.GetComponent<ParticleSystem>();
         //Debug.Log(ps);
-        Destroy(this.gameObject, 0.2f);
-
+        
     }
 }
