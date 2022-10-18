f47a1a2b140f26c13103dbc322c50363eac57849
prashantplusplus <81324922+prashantplusplus@users.noreply.github.com>
2022-10-18 04:30:13 -0700

    WordBlank Fix

--- Assets/scripts/bow_Level1.cs
+++ Assets/scripts/bow_Level1.cs
@@ -58,8 +58,9 @@ public class bow_Level1 : MonoBehaviour
 
     IEnumerator faceMouse()
     {
-        Vector2 startPosition = transform.right;
-        transform.right = Vector2.Lerp(startPosition, direction, Time.deltaTime / slowedFactor);
+        // Vector2 startPosition = transform.right;
+        // transform.right = Vector2.Lerp(startPosition, direction, Time.deltaTime / slowedFactor);
+        transform.right = direction;
         yield return null;
     }
 }
