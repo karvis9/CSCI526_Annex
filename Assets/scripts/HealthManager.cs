e7f8cc702d2763399a93fe12dd753ac92911e126
Vishal Kartik <karanam@usc.edu>
2022-11-06 13:50:19 -0800

    Adding health for the hangman part of the game (#86)
    having 5 health bars, and each will decrease when a character not a part of the word is shot. (Similar to hangman). The UI is not correctly in canvas as we need to move it to the panel ultimately.

---
+++ Assets/scripts/HealthManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager ht;
+    public GameObject heart_health_1, heart_health_2, heart_health_3, heart_health_4, heart_health_5;
+    public static int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 5;
+        heart_health_1.gameObject.SetActive(true);
+        heart_health_2.gameObject.SetActive(true);
+        heart_health_3.gameObject.SetActive(true);
+        heart_health_4.gameObject.SetActive(true);
+        heart_health_5.gameObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health > 5)
+        {
+            health = 5;
+        }
+        switch (health)
+        {
+            case 5:
+                heart_health_1.gameObject.SetActive(true);
+                heart_health_2.gameObject.SetActive(true);
+                heart_health_3.gameObject.SetActive(true);
+                heart_health_4.gameObject.SetActive(true);
+                heart_health_5.gameObject.SetActive(true);
+                break;
+            case 4:
+                heart_health_1.gameObject.SetActive(true);
+                heart_health_2.gameObject.SetActive(true);
+                heart_health_3.gameObject.SetActive(true);
+                heart_health_4.gameObject.SetActive(true);
+                heart_health_5.gameObject.SetActive(false);
+                break;
+            case 3:
+                heart_health_1.gameObject.SetActive(true);
+                heart_health_2.gameObject.SetActive(true);
+                heart_health_3.gameObject.SetActive(true);
+                heart_health_4.gameObject.SetActive(false);
+                heart_health_5.gameObject.SetActive(false);
+                break;
+            case 2:
+                heart_health_1.gameObject.SetActive(true);
+                heart_health_2.gameObject.SetActive(true);
+                heart_health_3.gameObject.SetActive(false);
+                heart_health_4.gameObject.SetActive(false);
+                heart_health_5.gameObject.SetActive(false);
+                break;
+            case 1:
+                heart_health_1.gameObject.SetActive(true);
+                heart_health_2.gameObject.SetActive(false);
+                heart_health_3.gameObject.SetActive(false);
+                heart_health_4.gameObject.SetActive(false);
+                heart_health_5.gameObject.SetActive(false);
+                break;
+            case 0:
+                heart_health_1.gameObject.SetActive(false);
+                heart_health_2.gameObject.SetActive(false);
+                heart_health_3.gameObject.SetActive(false);
+                heart_health_4.gameObject.SetActive(false);
+                heart_health_5.gameObject.SetActive(false);
+                GameOverScreen.EndGame(ScoreManager.sm.getFinalScore(), false, WordBlanks.wb.word);
+                Time.timeScale = 0;
+                break;
+
+        }
+        
+
+    }
+    public void decreaseHealth()
+    {
+        health = health-1;
+    }
+}
