728f8f9f40740dbe2451b90f6bad4583f24ed64c
rchanumo <scrs.ravi14@gmail.com>
2022-11-07 22:04:10 -0800

    fix cavas

--- Assets/BubbleCounter.cs
+++ Assets/BubbleCounter.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class BubbleCounter : MonoBehaviour
 {
-    public TextMeshPro counter;
+    public Text counter;
     // Start is called before the first frame update
     void Start()
     {
-        counter = this.gameObject.GetComponent<TextMeshPro>();
-        Debug.Log(counter);
+        //counter = this.gameObject.GetComponent<TextMeshPro>();
+        //Debug.Log(counter);
     }
 
     // Update is called once per frame
