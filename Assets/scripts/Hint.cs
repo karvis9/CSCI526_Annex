1b5c73d3cdc72eb46d3936350908529c91ac446d
Raveendra Shivalingayya Gachchinamath <gachchin@usc.edu>
2022-11-13 14:40:57 -0800

    really done

--- Assets/scripts/Hint.cs
+++ Assets/scripts/Hint.cs
@@ -12,17 +12,10 @@ public class Hint : MonoBehaviour
         gm.SetActive(false);
     }
     // Start is called before the first frame update
-    public void showHint(string curLevel_imageName){
+    public void showHint(string imageName){
         gm.SetActive(true);
-        string curLevelScene = curLevel_imageName.Split(",")[0];
-        string imageName = curLevel_imageName.Split(",")[1];
         Sprite hintImage = Resources.Load<Sprite>("Images/Hints/" + imageName);
         image.GetComponent<Image>().sprite = hintImage;
-        // PlayerPrefs.SetString("curLevelScene", curLevelScene);
-        // PlayerPrefs.SetString("imageName", imageName);
-        // PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex);
-        // PlayerPrefs.Save();
-        // SceneManager.LoadScene("HintScene");
     }
 
     public void closeHint() {
