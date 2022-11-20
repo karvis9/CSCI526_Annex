da044ae41dc963e62316eae342d1cda4f495c7b0
Arjun Chaturvedi <chaturvedi.arjun07@yahoo.co.in>
2022-11-20 14:03:09 -0800

    Tutorial Update.

--- Assets/scripts/TutorialController.cs
+++ Assets/scripts/TutorialController.cs
@@ -7,12 +7,13 @@ public class TutorialController : MonoBehaviour
 {
     public GameObject mask;
     public GameObject textbox;
+    public GameObject pointer;
 
     string visibleController = "";
     // Start is called before the first frame update
     void Start()
     {
-
+        pointer.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,14 +28,33 @@ public class TutorialController : MonoBehaviour
                 mask.transform.position = player.transform.position;
                 mask.transform.localScale = new Vector3(0.1f, 0.1f, 0);
                 visibleController = "Player";
-                PauseGame(5f);
+                PauseGame(3f);
             }
         }
+
         else if (visibleController.Equals("Player"))
         {
             if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
             {
+                mask.SetActive(false);
                 textbox.GetComponent<TMP_Text>().text = "Select upto 10 characters to spawn for shooting.";
+                pointer.SetActive(true);
+                visibleController = "Canvas";
+                PauseGame(3f);
+            }
+        }
+
+        else if (visibleController.Equals("Canvas"))
+        {
+            if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+            {
+                pointer.SetActive(false);
+                textbox.GetComponent<TMP_Text>().text = "";
+                mask.SetActive(true);
+                GameObject player = GameObject.FindGameObjectWithTag("Health");
+                mask.transform.position = player.transform.position;
+                mask.transform.localScale = new Vector3(0.1f, 0.1f, 0);
+                //textbox.GetComponent<TMP_Text>().text = "Life";
             }
         }
     }
